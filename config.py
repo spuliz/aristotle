b83011b1714a38a659a7e697459dda6d026669c6
Saverio Pulizzi <saverio.pulizzi91@gmail.com>
2020-05-22 19:17:06 +0100

    hello

--- config.py
+++ config.py
@@ -3,4 +3,7 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or "secret_string"
     # add DB connection here
-    MONGODB_SETTINGS = {'db' : 'UTA_Test'}
+    MONGODB_SETTINGS = {
+        'host': 'mongodb+srv://twidia:Kalispera2020@cluster0-3skvg.mongodb.net/whatsapp_db?retryWrites=true&w=majority',
+        'db' : 'whatsapp_db'
+        }
